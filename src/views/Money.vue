17d524869504181036711a95348ef692e87649b7
Righthand5 <xyx1530966653@163.com>
2020-10-31 12:10:05 +0800

    布局money页面

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
+  <div class="nav-wrapper">
+    <div class="content">
     money.vue
+  </div>
   <Nav/>
   </div>
 </template>
@@ -12,5 +14,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.nav-wrapper{
+  border: 1px solid green;
+  display: flex;
+  flex-direction:column;
+  height:100vh;
+}
+.content{
+  overflow: auto;
+  flex-grow:1;//让蓝色的地方尽量占满
+}
 </style>
