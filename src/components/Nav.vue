abf95e61e841253f2b1fa79b1c28451bd363b016
Righthand5 <xyx1530966653@163.com>
2020-11-02 14:10:32 +0800

    创建<icon /

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,21 +1,40 @@
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>
+    <router-link to="/money">
+      <Icon name="money"/>
+      记账</router-link>
     |
-    <router-link to="/labels">标签</router-link>
+    <router-link to="/labels">
+      <Icon name="label"/>
+      标签
+    </router-link>
     |
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/statistics">
+      <Icon name="statistics"/>
+      统计
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
+  /*一次性引入所有svg*/
+  // const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+  // try {
+  //   importAll(require.context('../assets/icons', true, /\.svg$/));
+  // }catch (error){
+  //   console.log(error)
+  // }
+import x from '@/assets/icons/label.svg';
+import y from '@/assets/icons/money.svg';
+console.log(x);
+console.log(y);
 export default {
-  name: 'Nav'
+  name: 'Nav',
 };
 </script>
 
 <style lang="scss" scoped>
-  .nav{
-    border: 1px solid red;
-  }
+.nav {
+  border: 1px solid red;
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,25 @@
+const path = require('path')
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  chainWebpack:config=>{
+    const dir = path.resolve(__dirname, 'src/assets/icons')
+    /*获取你想要请求的文件*/
+    /*webpack被vue封装后暴露的一个config对象*/
+    config.module
+        .rule('svg-sprite')
+        .test(/\.svg$/)
+        /*只包含dir(icons)的目录*/
+        .include.add(dir).end()
+        .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract:false}).end()
+    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'),[{plainSprite: true}])
+    /*其他svg loader排除icons目录*/
+    config.module.rule('svg').exclude.add(dir)
+
+
+
+
+
+
+  }
 }
