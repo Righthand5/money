8578e618ae166d69d78db9bb8853a50e2007bc1c
Righthand5 <xyx1530966653@163.com>
2020-11-02 14:40:49 +0800

    改变导航栏样式

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -16,13 +16,11 @@
 
 <style lang="scss" scoped>
 .nav-wrapper{
-  border: 1px solid green;
   display: flex;
   flex-direction:column;
   height:100vh;
 }
 .content{
-  border:1px solid blue;
   overflow: auto;
   flex-grow:1;//让蓝色的地方尽量占满
 }
